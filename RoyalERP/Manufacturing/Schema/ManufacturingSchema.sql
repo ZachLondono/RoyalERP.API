c9c058c5c596353a6615caa8889701ccd929de6c
ZachLondono <zachary.londono5@gmail.com>
2022-07-28 16:50:03 -0400

    Update schema, released date should not default to current timestamp

--- RoyalERP/Manufacturing/Schema/ManufacturingSchema.sql
+++ RoyalERP/Manufacturing/Schema/ManufacturingSchema.sql
@@ -4,7 +4,7 @@ CREATE TABLE manufacturing.workorders (
     id uuid NOT NULL,
     number character varying(255) NOT NULL,
     name character varying(255) NOT NULL,
-    releaseddate timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    releaseddate timestamp with time zone,
     fulfilleddate timestamp with time zone,
     status character varying(255) NOT NULL,
     CONSTRAINT companies_pkey PRIMARY KEY (id)
