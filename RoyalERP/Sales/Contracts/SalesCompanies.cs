ef8408975c9d83e0d3e25b8d8916ce04b4eeec9b
ZachLondono <zachary.londono5@gmail.com>
2022-07-30 01:58:02 -0400

    Created delegates for querying sales context

---
+++ RoyalERP/Sales/Contracts/SalesCompanies.cs
@@ -0,0 +1,9 @@
+﻿using RoyalERP.Sales.Companies.DTO;
+
+namespace RoyalERP.Sales.Contracts;
+
+public static class SalesCompanies {
+
+    public delegate Task<CompanyDTO?> GetCompanyById(Guid Id);
+
+}
