11d84c7bc96bcffef42a9b9dd4f21bd6cae4a1b1
ZachLondono <zachary.londono5@gmail.com>
2022-07-30 01:58:27 -0400

    Created implementation for sales context queries

---
+++ RoyalERP/Sales/SalesQueries.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using RoyalERP.Sales.Companies.DTO;
+using RoyalERP.Sales.Orders.DTO;
+
+namespace RoyalERP.Sales;
+
+public class SalesQueries {
+
+    private readonly ISender _sender;
+
+    public SalesQueries(ISender sender) {
+        _sender = sender;
+    }
+
+    public Task<OrderDTO?> GetByOrderId(Guid id) {
+        return _sender.Send(new Orders.Queries.GetById.Query(id));
+    }
+
+    public Task<CompanyDTO?> GetByCompanyId(Guid id) {
+        return _sender.Send(new Companies.Queries.GetById.Query(id));
+    }
+
+}
