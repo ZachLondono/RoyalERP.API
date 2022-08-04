8dca6b9879d60bd81bb45ac006b538f31e35019b
ZachLondono <zachary.londono5@gmail.com>
2022-08-03 22:59:26 -0400

    Fixed null warning

--- RoyalERP_IntegrationTests/TestUtils.cs
+++ RoyalERP_IntegrationTests/TestUtils.cs
@@ -46,7 +46,7 @@ public static class TestUtils {
 
     }
 
-    public static DirectoryInfo? TryGetSolutionDirectoryInfo(string currentPath = null) {
+    public static DirectoryInfo? TryGetSolutionDirectoryInfo(string? currentPath = null) {
         var directory = new DirectoryInfo(
             currentPath ?? Directory.GetCurrentDirectory());
         while (directory != null && !directory.GetFiles("*.sln").Any()) {
