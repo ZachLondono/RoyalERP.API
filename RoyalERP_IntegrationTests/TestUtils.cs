ed38a51b3c4e6cfbfc7446450066a079105c90f2
ZachLondono <zachary.londono5@gmail.com>
2022-08-03 22:58:09 -0400

    Switched to using a relative path from the solution file for sql scripts

--- RoyalERP_IntegrationTests/TestUtils.cs
+++ RoyalERP_IntegrationTests/TestUtils.cs
@@ -1,22 +1,28 @@
 ﻿using Npgsql;
 using System.Data;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RoyalERP_IntegrationTests;
 
 public static class TestUtils {
 
-    private static readonly string _salesSchemaPath = @"C:\Users\Zachary Londono\source\repos\RoyalERP\RoyalERP\Sales\Schema\SalesSchema.sql";
-    private static readonly string _manufSchemaPath = @"C:\Users\Zachary Londono\source\repos\RoyalERP\RoyalERP\Manufacturing\Schema\ManufacturingSchema.sql";
-
     public static async Task CreateTables(string connectionString) {
+
+        var directory = TryGetSolutionDirectoryInfo();
+        if (directory is null)
+            throw new InvalidDataException("Could not find path to db schema");
+
+        var salesSchemaPath = Path.Combine(directory.FullName, @"RoyalERP\Sales\Schema\SalesSchema.sql");
+        var manufSchemaPath = Path.Combine(directory.FullName, @"RoyalERP\Manufacturing\Schema\ManufacturingSchema.sql");
 
         using var connection = new NpgsqlConnection(connectionString);
 
         connection.Open();
 
-        await ExecuteScript(connection, _salesSchemaPath);
-        await ExecuteScript(connection, _manufSchemaPath);
+        await ExecuteScript(connection, salesSchemaPath);
+        await ExecuteScript(connection, manufSchemaPath);
 
         connection.Close();        
 
@@ -38,6 +44,15 @@ public static class TestUtils {
 
         if (closeConnection) connection.Close();
 
+    }
+
+    public static DirectoryInfo? TryGetSolutionDirectoryInfo(string currentPath = null) {
+        var directory = new DirectoryInfo(
+            currentPath ?? Directory.GetCurrentDirectory());
+        while (directory != null && !directory.GetFiles("*.sln").Any()) {
+            directory = directory.Parent;
+        }
+        return directory;
     }
 
 }
