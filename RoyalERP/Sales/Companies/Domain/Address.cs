938f894ffad7e6c8050a6a6f8d10c1f9c9978e28
ZachLondono <zachary.londono5@gmail.com>
2022-08-10 21:57:38 -0400

    Added address class and added address to company. As well as methods for updating company.

---
+++ RoyalERP/Sales/Companies/Domain/Address.cs
@@ -0,0 +1,19 @@
+﻿namespace RoyalERP.Sales.Companies.Domain;
+
+public class Address {
+
+    public Guid Id { get; set; }
+
+    public string Line1 { get; set; } = string.Empty;
+
+    public string Line2 { get; set; } = string.Empty;
+
+    public string Line3 { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string State { get; set; } = string.Empty;
+
+    public string Zip { get; set; } = string.Empty;
+
+}
--- RoyalERP/Sales/Companies/Domain/Company.cs
+++ RoyalERP/Sales/Companies/Domain/Company.cs
@@ -6,14 +6,40 @@ public class Company : AggregateRoot {
 
     public string Name { get; private set; }
 
-    public Company(Guid id, int version, string name) : base(id, version) {
+    public string Contact { get; private set; }
+
+    public string Email { get; private set; }
+
+    public Address Address { get; private set; }
+
+    public Company(Guid id, int version, string name, string contact, string email, Address address) : base(id, version) {
         Name = name;
+        Contact = contact;
+        Email = email;
+        Address = address;
     }
 
-    private Company(string name) : this(Guid.NewGuid(), 0, name) {
+    private Company(string name) : this(Guid.NewGuid(), 0, name, "", "", new()) {
         AddEvent(new Events.CompanyCreatedEvent(Id, name));
     }
 
     public static Company Create(string name) => new(name);
+
+    public void Update(string name, string contact, string email) {
+        Name = name;
+        Contact = contact;
+        Email = email;
+        AddEvent(new Events.CompanyUpdatedEvent(Id, name, contact, email));
+    }
+
+    public void SetAddress(string line1, string line2, string line3, string city, string state, string zip) {
+        Address.Line1 = line1;
+        Address.Line2 = line2;
+        Address.Line3 = line3;
+        Address.City = city;
+        Address.State = state;
+        Address.Zip = zip;
+        AddEvent(new Events.CompanyAddressUpdatedEvent(Id, Address.Id, line1, line2, line3, city, state, zip));
+    }
 
 }
