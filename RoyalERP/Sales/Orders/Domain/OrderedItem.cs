fef30698db517be5c677fe2bff8d5872d3e054d1
ZachLondono <zachary.londono5@gmail.com>
2022-08-12 00:06:04 -0400

    Created ordered item and added it to order

---
+++ RoyalERP/Sales/Orders/Domain/OrderedItem.cs
@@ -0,0 +1,28 @@
+﻿using RoyalERP.Common.Domain;
+
+namespace RoyalERP.Sales.Orders.Domain;
+
+public class OrderedItem : Entity {
+
+    public Guid OrderId { get; init; }
+
+    public string ProductName { get; init; }
+
+    public int Quantity { get; init; }
+
+    public Dictionary<string, string> Properties { get; init; }
+
+    public OrderedItem(Guid id, Guid orderId, string productName, int quantity, Dictionary<string, string> properties) : base(id) {
+        OrderId = orderId;
+        ProductName = productName;
+        Quantity = quantity;
+        Properties = properties;
+    }
+
+    private OrderedItem(Guid orderId, string productName, int quantity, Dictionary<string, string> properties) : this(Guid.NewGuid(), orderId, productName, quantity, properties) {
+        AddEvent(new Events.OrderedItemCreated(Id, orderId, productName, quantity, properties));
+    }
+
+    public static OrderedItem Create(Guid orderId, string productName, int quantity, Dictionary<string, string> properties) => new(orderId, productName, quantity, properties);
+
+}
