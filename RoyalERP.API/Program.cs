928d618c7efc724fb4285361cea4c71ba2002893
ZachLondono <zachary.londono5@gmail.com>
2022-08-19 13:03:50 -0400

    Added catalog to sevices

--- RoyalERP.API/Program.cs
+++ RoyalERP.API/Program.cs
@@ -6,6 +6,7 @@ using RoyalERP.Common.Domain;
 using RoyalERP.API.Manufacturing;
 using RoyalERP.API.Sales;
 using System.Text.Json.Serialization;
+using RoyalERP.API.Catalog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,8 +19,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(typeof(Program).Assembly);
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-builder.Services.AddManufacturing();
-builder.Services.AddSales();
+
+builder.Services.AddManufacturing()
+                .AddSales()
+                .AddCatalog();
 
 SqlMapper.AddTypeHandler(new JsonTypeHandler<Dictionary<string,string>>());
 
