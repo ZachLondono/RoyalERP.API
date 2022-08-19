0b9be70b83c35dab3466e3eafd3c78b475f4c17e
ZachLondono <zachary.londono5@gmail.com>
2022-08-19 12:29:22 -0400

    Added connection factory and extension method for adding catalog

---
+++ RoyalERP.API/Catalog/ConfigureServices.cs
@@ -0,0 +1,12 @@
+﻿namespace RoyalERP.API.Catalog;
+
+public static class ConfigureServices {
+
+    public static IServiceCollection AddCatalog(this IServiceCollection services) {
+
+        services.AddTransient<ICatalogConnectionFactory, NpgsqlCatalogConnectionFactory>();
+
+        return services;
+
+    }
+}
---
+++ RoyalERP.API/Catalog/NpgsqlCatalogConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace RoyalERP.API.Catalog;
+
+public class NpgsqlCatalogConnectionFactory : ICatalogConnectionFactory {
+    
+    private readonly IConfiguration _config;
+
+    public NpgsqlCatalogConnectionFactory(IConfiguration config) {
+        _config = config;
+    }
+
+    public IDbConnection CreateConnection() {
+        string connectionString = _config.GetConnectionString("CatalogDatabase");
+        return new NpgsqlConnection(connectionString);
+    }
+
+}
