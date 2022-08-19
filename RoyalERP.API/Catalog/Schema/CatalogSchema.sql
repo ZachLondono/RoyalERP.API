8baa8efa1b3c764dd5739239b88ee04dcdf61b08
ZachLondono <zachary.londono5@gmail.com>
2022-08-18 23:35:24 -0400

    Added catalog schema

---
+++ RoyalERP.API/Catalog/Schema/CatalogSchema.sql
@@ -0,0 +1,28 @@
+﻿CREATE SCHEMA catalog;
+
+CREATE TABLE catalog.productclasses (
+    id uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    CONSTRAINT productclasses_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE catalog.products (
+    id uuid NOT NULL,
+    classid uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE catalog.productattributes (
+    id uuid NOT NULL,
+    productid uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    CONSTRAINT productattributes_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE catalog.product_productattribute (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 ),
+    productid uuid NOT NULL,
+    attributeid uuid NOT NULL,
+    CONSTRAINT product_productattribute_pkey PRIMARY KEY (id)
+);
