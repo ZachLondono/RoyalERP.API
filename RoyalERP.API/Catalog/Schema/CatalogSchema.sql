dda000b6144a61bc154eac0bd484f426c52232bd
ZachLondono <zachary.londono5@gmail.com>
2022-08-19 15:15:28 -0400

    Updated schema to use an array of ids instead of a seperate table

--- RoyalERP.API/Catalog/Schema/CatalogSchema.sql
+++ RoyalERP.API/Catalog/Schema/CatalogSchema.sql
@@ -6,13 +6,6 @@ CREATE TABLE catalog.productclasses (
     CONSTRAINT productclasses_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE catalog.products (
-    id uuid NOT NULL,
-    classid uuid NOT NULL,
-    name character varying(255) NOT NULL,
-    CONSTRAINT products_pkey PRIMARY KEY (id)
-);
-
 CREATE TABLE catalog.productattributes (
     id uuid NOT NULL,
     productid uuid NOT NULL,
@@ -20,9 +13,10 @@ CREATE TABLE catalog.productattributes (
     CONSTRAINT productattributes_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE catalog.product_productattribute (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 ),
-    productid uuid NOT NULL REFERENCES catalog.products(id) ON DELETE CASCADE,
-    attributeid uuid NOT NULL REFERENCES catalog.productattributes(id) ON DELETE CASCADE,,
-    CONSTRAINT product_productattribute_pkey PRIMARY KEY (id)
+CREATE TABLE catalog.products (
+    id uuid NOT NULL,
+    classid uuid REFERENCES catalog.productclasses(id),
+    name character varying(255) NOT NULL,
+    attributeids uuid[] NOT NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
 );
