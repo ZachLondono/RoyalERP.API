0d44f3060dfc777e809c3bbcdaaea46a8a6ab72c
ZachLondono <zachary.londono5@gmail.com>
2022-08-20 13:25:07 -0400

    configuring unit of work

--- RoyalERP.API/Catalog/ConfigureServices.cs
+++ RoyalERP.API/Catalog/ConfigureServices.cs
@@ -1,10 +1,23 @@
-﻿namespace RoyalERP.API.Catalog;
+﻿using RoyalERP.API.Catalog.ProductAttributes.Domain;
+using RoyalERP.API.Catalog.ProductClasses.Domain;
+using RoyalERP.API.Catalog.Products.Domain;
+using RoyalERP.Common.Data;
+using System.Data;
+
+namespace RoyalERP.API.Catalog;
 
 public static class ConfigureServices {
 
     public static IServiceCollection AddCatalog(this IServiceCollection services) {
 
         services.AddTransient<ICatalogConnectionFactory, NpgsqlCatalogConnectionFactory>();
+
+        services.AddSingleton<Func<IDbConnection, IDbTransaction, IProductRepository>>(s => (c, t) => new ProductRepository(new DapperConnection(c), t));
+        services.AddSingleton<Func<IDbConnection, IDbTransaction, IProductClassRepository>>(s => (c, t) => new ProductClassRepository(new DapperConnection(c), t));
+        services.AddSingleton<Func<IDbConnection, IDbTransaction, IProductAttributeRepository>>(s => (c, t) => new ProductAttributeRepository(new DapperConnection(c), t));
+
+        services.AddTransient<ICatalogUnitOfWork, CatalogUnitOfWork>();
+
 
         return services;
 
--- RoyalERP.API/Catalog/ICatalogUnitOfWork.cs
+++ RoyalERP.API/Catalog/ICatalogUnitOfWork.cs
@@ -1,9 +1,14 @@
-﻿using RoyalERP.API.Catalog.Products.Domain;
+﻿using RoyalERP.API.Catalog.ProductAttributes.Domain;
+using RoyalERP.API.Catalog.ProductClasses.Domain;
+using RoyalERP.API.Catalog.Products.Domain;
+using RoyalERP.Common.Domain;
 
 namespace RoyalERP.API.Catalog;
 
-public interface ICatalogUnitOfWork {
+public interface ICatalogUnitOfWork : IUnitOfWork {
 
     IProductRepository Products { get; }
+    IProductClassRepository ProductClasses { get; }
+    IProductAttributeRepository ProductAttributes { get; }
 
 }
