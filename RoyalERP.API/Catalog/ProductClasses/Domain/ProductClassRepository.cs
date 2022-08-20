fc8d993511dbcd8f145350e5f3c3c9fb875b2b6c
ZachLondono <zachary.londono5@gmail.com>
2022-08-20 01:03:27 -0400

    Implemented repository

--- RoyalERP.API/Catalog/ProductClasses/Domain/ProductClassRepository.cs
+++ RoyalERP.API/Catalog/ProductClasses/Domain/ProductClassRepository.cs
@@ -1,31 +1,59 @@
 ﻿using MediatR;
+using RoyalERP.API.Catalog.Products.Domain;
+using RoyalERP.Common.Data;
+using System.Data;
 
 namespace RoyalERP.API.Catalog.ProductClasses.Domain;
 
 public class ProductClassRepository : IProductClassRepository {
 
+    private readonly IDapperConnection _connection;
+    private readonly IDbTransaction _transaction;
+
+    private readonly List<ProductClass> _activeEntities = new();
+
+    public ProductClassRepository(IDapperConnection connection, IDbTransaction transaction) {
+        _connection = connection;
+        _transaction = transaction;
+    }
+
     public Task AddAsync(ProductClass entity) {
-        throw new NotImplementedException();
+
+        const string command = "INSERT INTO catalog.productclasses (id, version, name) VALUES (@Id, @Version, @Name);";
+
+        return _connection.ExecuteAsync(sql: command, transaction: _transaction, param: new {
+            entity.Id,
+            entity.Version,
+            entity.Name
+        });
+
     }
 
     public Task<IEnumerable<ProductClass>> GetAllAsync() {
-        throw new NotImplementedException();
+        const string command = "SELECT id, version, name FROM catalog.productclasses;";
+        return _connection.QueryAsync<ProductClass>(sql: command, transaction: _transaction);
     }
 
     public Task<ProductClass?> GetAsync(Guid id) {
-        throw new NotImplementedException();
+        const string command = "SELECT id, version, name FROM catalog.productclasses WHERE id = @Id;";
+        return _connection.QuerySingleOrDefaultAsync<ProductClass?>(sql: command, transaction: _transaction, param: new { Id = id });
     }
 
     public Task RemoveAsync(ProductClass entity) {
-        throw new NotImplementedException();
+        _activeEntities.Remove(entity);
+        const string command = "DELETE FROM catalog.productclasses WHERE id = @Id;";
+        return _connection.ExecuteAsync(sql: command, transaction: _transaction, param: new { Id = entity.Id });
     }
 
     public Task UpdateAsync(ProductClass entity) {
-        throw new NotImplementedException();
+        const string command = "DELETE FROM catalog.productclasses WHERE id = @Id;";
+        return _connection.ExecuteAsync(sql: command, transaction: _transaction, param: new { Id = entity.Id });
     }
 
-    public Task PublishEvents(IPublisher publisher) {
-        throw new NotImplementedException();
+    public async Task PublishEvents(IPublisher publisher) {
+        foreach(var entity in _activeEntities) {
+            await entity.PublishEvents(publisher);
+        }
     }
 
 }
