8f049c861365de8b9c1974785298e69efce823ad
ZachLondono <zachary.londono5@gmail.com>
2022-08-20 12:27:38 -0400

    Query can return a null value

--- RoyalERP.API/Common/Data/IDapperConnection.cs
+++ RoyalERP.API/Common/Data/IDapperConnection.cs
@@ -5,6 +5,6 @@ namespace RoyalERP.Common.Data;
 public interface IDapperConnection {
     Task<int> ExecuteAsync(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null);
     Task<IEnumerable<T>> QueryAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null);
-    Task<T> QuerySingleOrDefaultAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+    Task<T?> QuerySingleOrDefaultAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null, int? commandTimeout = null, CommandType? commandType = null);
     Task<IEnumerable<T3>> QueryAsync<T1, T2, T3>(string sql, Func<T1, T2, T3> map, object? param = null, IDbTransaction? transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null);
 }
